8f3aeea761364f667eace92358a00c37f1d7d6d7
parasharparth <parthparashar6@gmail.com>
2022-01-19 16:18:28 -0800

    Airline code

--- airline/src/test/java/edu/pdx/cs410J/parth2/Project1Test.java
+++ airline/src/test/java/edu/pdx/cs410J/parth2/Project1Test.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
+import edu.pdx.cs410J.InvokeMainTestCase;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
