df2974bf30850ab9fdd26244001aa2c5a97d0211
parasharparth <parthparashar6@gmail.com>
2022-01-26 16:34:01 -0800

    Project-2 Initial commit along with Project-1 mistakes rectification

---
+++ airline/src/test/java/edu/pdx/cs410J/parth2/Project2Test.java
@@ -0,0 +1,10 @@
+package edu.pdx.cs410J.parth2;
+import edu.pdx.cs410J.InvokeMainTestCase;
+
+public class Project2Test extends InvokeMainTestCase {
+
+
+    private MainMethodResult invokeMain(String... args) {
+        return invokeMain( Project2.class, args );
+    }
+}
