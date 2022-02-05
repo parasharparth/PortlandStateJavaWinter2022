b7bf18f44beb8148682d0a6dc2c7f648cfe6a578
parasharparth <parthparashar6@gmail.com>
2022-02-04 16:25:55 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutEquality.java
+++ koans/src/beginner/AboutEquality.java
@@ -35,7 +35,7 @@ public class AboutEquality {
     public void equalsMethodCanBeChangedBySubclassesToTestsIfTwoObjectsAreEqualExample() {
         Integer value1 = new Integer(4);
         Integer value2 = new Integer(2 + 2);
-        assertEquals(value1.equals(value2), __);
+        assertEquals(value1.equals(value2), true);
         assertEquals(value1, __);
     }
 
