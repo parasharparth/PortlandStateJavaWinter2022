f3c324a6e8f296df6d104b6ec622d5fe81272350
parasharparth <parthparashar6@gmail.com>
2022-02-05 03:14:04 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutEnums.java
+++ koans/src/beginner/AboutEnums.java
@@ -24,7 +24,7 @@ public class AboutEnums {
     @Koan
     public void basicEnumsAccess() {
         Colors[] colorArray = Colors.values();
-        assertEquals(colorArray[2], __);
+        assertEquals(colorArray[2], "Green");
     }
 
     enum SkatSuits {
