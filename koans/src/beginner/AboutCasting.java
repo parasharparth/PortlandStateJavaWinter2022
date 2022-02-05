c8b45ae0b028513a80483f05053c3bb41a691be7
parasharparth <parthparashar6@gmail.com>
2022-02-05 02:46:13 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -14,7 +14,7 @@ public class AboutCasting {
         int a = 6;
         long b = 10;
         Object c = a + b;
-        assertEquals(c, 16);
+        assertEquals(c, (Object)16);
         assertEquals(c instanceof Integer, true);
         assertEquals(c instanceof Long, false);
     }
