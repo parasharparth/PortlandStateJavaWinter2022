a4f0d695feefaba4cf3370d3bab603a80bd6dda5
parasharparth <parthparashar6@gmail.com>
2022-02-05 03:05:47 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -97,8 +97,8 @@ public class AboutCasting {
     @Koan
     public void classCasting() {
         try {
-            Object o = new Object();
-            ((Parent) o).sleep(); // would this even compile without the cast?
+            Grandparent o = new Grandparent();
+            o.sleep(); // would this even compile without the cast?
         } catch (ClassCastException x) {
             fail("Object does not implement Sleepable, maybe one of the people classes do?");
         }
