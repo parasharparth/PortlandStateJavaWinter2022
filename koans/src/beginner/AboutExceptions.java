ade78b6b48515ac093364c68b36e7449ae5ed6f1
parasharparth <parthparashar6@gmail.com>
2022-02-05 03:25:23 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutExceptions.java
+++ koans/src/beginner/AboutExceptions.java
@@ -145,7 +145,7 @@ public class AboutExceptions {
         } catch (IllegalArgumentException ex) {
             s = "caught an IllegalArgumentException";
         }
-        assertEquals(s, __);
+        assertEquals(s, "caught an IllegalArgumentException");
     }
 
     @Koan
