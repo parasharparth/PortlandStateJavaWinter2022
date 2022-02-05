d47ec3a1a491aa25dacf19dbc14196a12e5ea8d4
parasharparth <parthparashar6@gmail.com>
2022-02-05 02:57:37 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -98,7 +98,7 @@ public class AboutCasting {
     public void classCasting() {
         try {
             Object o = new Object();
-            ((Sleepable) o).sleep(); // would this even compile without the cast?
+            ((Grandparent) o).sleep(); // would this even compile without the cast?
         } catch (ClassCastException x) {
             fail("Object does not implement Sleepable, maybe one of the people classes do?");
         }
@@ -108,7 +108,7 @@ public class AboutCasting {
     public void complicatedCast() {
         Grandparent parent = new Parent();
         // How can we access the parent's ability to "complain" - if the reference is held as a superclass?
-        assertEquals("TPS reports don't even have a cover letter!", __);
+        assertEquals("TPS reports don't even have a cover letter!", true);
     }
 
 }
