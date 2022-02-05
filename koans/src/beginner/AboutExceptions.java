83dc1cf2421e89da403d3ed8b8c03b01dd804720
parasharparth <parthparashar6@gmail.com>
2022-02-05 03:18:35 -0800

    sixth Lesson commit test./

--- koans/src/beginner/AboutExceptions.java
+++ koans/src/beginner/AboutExceptions.java
@@ -22,7 +22,7 @@ public class AboutExceptions {
         } catch (IOException e) {
             s = "exception thrown";
         }
-        assertEquals(s, __);
+        assertEquals(s, "exception thrown");
     }
 
     @Koan
